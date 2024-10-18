dd87f895a928c957af1f9e433391448aef81a3a5
Raphael-08 <mohanvarma130@gmail.com>
2024-10-18 21:03:50 +0530

    login page complete

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "@/components/login";
+
+export default function Login() {
+  return (
+    <div className="grid items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <LoginForm />
+    </div>
+  );
+}
